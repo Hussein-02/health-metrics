88652c77b6932e9772af5d7b9db0b887865feefc
Hussein-02 <hussein02ab@gmail.com>
2025-03-31 21:26:30 +0300

    Added jwt and finished register function

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(['success' => 'true', 'user' => $user, 'token' => $token], 201);
+    }
 }
